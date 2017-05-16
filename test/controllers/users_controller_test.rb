8728184ff6e6c58dccf49e22e5b56938e2ddb167
Kozo Komiya <kozo.komiya@gmail.com>
2017-05-16 15:14:38 +0000

    add destroy action completely

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,7 +5,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
     @other_user = users(:archer)
   end
-  
 
   test "should redirect index when not logged in" do
     get users_path
@@ -28,6 +27,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
   end
 
 end
